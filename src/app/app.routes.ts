8fbcab6ceeba187e841c5805f26ce4a2237c4083
leoalevalle2014 <leoalevalle2014@gmail.com>
2026-05-02 19:57:57 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/header',
+        pathMatch: 'full'
+    },
+    {
+        path: 'header',
+        loadComponent: () => import('./components/header/header').then(m => m.Header)
+    }
+];
---
+++ src/app/components/punto4/punto4.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-punto4',
+  imports: [],
+  templateUrl: './punto4.html',
+  styleUrl: './punto4.css',
+})
+export class Punto4 {}
